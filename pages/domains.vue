a09d3f3cbd7bf4fc948554c79b85cccd2cef3410
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-09-24 23:10:30 +0300

    Домены

--- pages/domains.vue
+++ pages/domains.vue
@@ -4,72 +4,134 @@
       <div class="container">
         <Nav/>
         <div class="lead">
-          <h1>Подбери свое доменное имя<br> в зоне .ru всего за 85 руб</h1>
-          <form action="">
-            <input type="text" placeholder="Имя домена">
-            <button type="submit">Подобрать домен</button>
+          <h1 v-html="translate.header.title"></h1>
+          <form @submit.prevent="gotoDomainReg()">
+            <input type="text" :placeholder="translate.header.placeholder" v-model="domain">
+            <button type="submit">{{ translate.header.button }}</button>
           </form>
         </div>
       </div>
     </header>
     <main>
       <section class="container domains-lead">
-        <strong>Уже есть свой<br> домен?</strong>
-        <a href="#">Перенеси его к нам</a>
+        <strong v-html="translate.lead.title"></strong>
+        <a target="_blank" :href="getDomainTransferLink()">{{ translate.lead.button }}</a>
       </section>
 
       <section class="container other-offers">
-        <h2 class="title center">Другие предложения</h2>
+        <h2 class="title center">{{ translate.offers.title }}</h2>
         <div class="row">
-          <div class="col-12 col-md-6 col-lg-4">
-            <strong>Название услуги <span>в две строки</span></strong>
+          <div
+            class="col-12 col-md-6 col-lg-4"
+            v-for="(el, i) in translate.offers.elements" :key="i"
+          >
+            <strong v-html="el.title"></strong>
             <ul class="list-unstyled">
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
+              <li v-for="(string, j) in el.list" :key="j">{{ string }}</li>
             </ul>
-            <a href="">Заказать услугу</a>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4">
-            <strong>Название услуги <span>в две строки</span></strong>
-            <ul class="list-unstyled">
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-            </ul>
-            <a href="">Заказать услугу</a>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 m-auto">
-            <strong>Название услуги <span>в две строки</span></strong>
-            <ul class="list-unstyled">
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-              <li>Какое то преимущество услуги</li>
-            </ul>
-            <a href="">Заказать услугу</a>
+            <a :href="el.link.href" target="_blank">{{ el.link.text }}</a>
           </div>
         </div>
       </section>
-
-      <section class="container feedback">
-        <div class="row">
-          <div class="col-12 col-md-6">
-            <h2 class="title">У вас еще остались <br>вопросы?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id</p>
-          </div>
-          <div class="col-12 col-md-6 col-lg-5 offset-lg-1">
-            <form action="">
-              <input type="text" placeholder="Ваше имя">
-              <input type="text" placeholder="Ваш Email">
-              <button type="submit">Получить консультацию</button>
-            </form>
-          </div>
-        </div>
-      </section>
+      <Feedback/>
     </main>
     <Footer/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      translate: translate,
+      domain: ''
+    }
+  },
+  methods: {
+    getDomainTransferLink: () => getISPDomainLink('transfer'),
+    gotoDomainReg: function() {
+      window.open(this.getDomainRegLink, '_blank')
+    }
+  },
+  computed: {
+    getDomainRegLink: function(): string {
+      return getISPDomainLink('register', this.domain)
+    }
+  }
+})
+
+let translate = {
+  header: {
+    title: 'Подбери свое доменное имя<br> в зоне .ru всего за 85 руб',
+    placeholder: 'Имя домена',
+    button: 'Подобрать домен'
+  },
+  lead: {
+    title: 'Уже есть свой<br> домен?',
+    button: 'Перенеси его к нам'
+  },
+  offers: {
+    title: 'Другие предложения',
+    elements: [
+      {
+        title: 'Название услуги <span>в две строки</span>',
+        list: [
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги'
+        ],
+        link: {
+          text: 'Заказать услугу',
+          href: '#'
+        }
+      },
+      {
+        title: 'Название услуги <span>в две строки</span>',
+        list: [
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги'
+        ],
+        link: {
+          text: 'Заказать услугу',
+          href: '#'
+        }
+      },
+      {
+        title: 'Название услуги <span>в две строки</span>',
+        list: [
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги',
+          'Какое то преимущество услуги'
+        ],
+        link: {
+          text: 'Заказать услугу',
+          href: '#'
+        }
+      }
+    ]
+  }
+}
+
+function getISPDomainLink(action: string, domain?: string) {
+  const queryParams = {
+    startpage: 'domain',
+    startform: 'domain.order.name',
+    domain_action: action,
+    domain_name: domain
+  }
+  if (domain == undefined) delete queryParams.domain_name
+
+  return `https://billing.spacecore.pro/billmgr?func=register&redirect=${encodeURIComponent(toQueryString(queryParams))}`
+}
+
+function toQueryString(object: any): string {
+  return Object.keys(object)
+    .map(key => `${key}=${object[key].toString()}`)
+    .join('&');
+}
+</script>
