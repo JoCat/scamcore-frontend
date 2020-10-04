1857fd10d0ce7b2ef23ed801885bc97eb8a36b1a
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-10-04 17:15:08 +0300

    Ласт правки

---
+++ pages/data-centers.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <header class="data-centers">
+      <div class="container">
+        <Nav/>
+        <div class="lead">
+            <h1 v-html="translate.header.title"></h1>
+            <p v-html="translate.header.description"></p>
+            <img src="/images/data-centers.png" alt="">
+        </div>
+      </div>
+    </header>
+    <main>
+      <section class="container data-centers-page">
+        <div class="card" v-for="(el, i) in translate.elements" :key="i">
+          <div class="card-title">
+            <strong>{{ el.name }}</strong>
+            <div>
+              <img :src="`/images/flags/${el.location.flag}.png`" alt="">
+              {{ el.location.city }} - <span>{{ el.location.country }}</span>
+            </div>
+          </div>
+          <div class="slider">
+            <img class="img-fluid" src="/images/dc-slider.png" alt="">
+          </div>
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="info" v-html="el.description"></div>
+            </div>
+            <div class="col-lg-8" v-html="el.content"></div>
+          </div>
+          <nuxt-link class="more" to="/dedicated">{{ translate.servers }}</nuxt-link>
+        </div>
+      </section>
+      <Faq/>
+      <Feedback/>
+    </main>
+    <Footer/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      translate: translate
+    }
+  }
+})
+
+let translate = {
+  header: {
+    title: 'Больше количество<br> дата центров',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br> tempor incididunt ut labore et dolore',
+  },
+  elements: [
+    {
+      name: 'Название цода',
+      location: {
+        flag: 'canada',
+        country: 'Ukraine',
+        city: 'Donbas'
+      },
+      image: [
+        '/images/dc-slider.png'
+      ],
+      description: `Москва, ул. Берзарина, д. 36, стр. 3<br><br>
+Площадь серверных помещений — 1 250 м²<br><br>
+Количество серверных стоек — 500<br><br>
+Мощность ДЦ, кВТ — 3 300<br><br>
+Источники бесперебойного питания, дизель-генераторы N+1, прямой фрикулинг с адиабатическим доохлаждением`,
+      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit animLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim`
+    },
+    {
+      name: 'Название цода',
+      location: {
+        flag: 'canada',
+        country: 'Ukraine',
+        city: 'Donbas'
+      },
+      image: [
+        '/images/dc-slider.png'
+      ],
+      description: `Москва, ул. Берзарина, д. 36, стр. 3<br><br>
+Площадь серверных помещений — 1 250 м²<br><br>
+Количество серверных стоек — 500<br><br>
+Мощность ДЦ, кВТ — 3 300<br><br>
+Источники бесперебойного питания, дизель-генераторы N+1, прямой фрикулинг с адиабатическим доохлаждением`,
+      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit animLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim`
+    },
+    {
+      name: 'Название цода',
+      location: {
+        flag: 'canada',
+        country: 'Ukraine',
+        city: 'Donbas'
+      },
+      image: [
+        '/images/dc-slider.png'
+      ],
+      description: `Москва, ул. Берзарина, д. 36, стр. 3<br><br>
+Площадь серверных помещений — 1 250 м²<br><br>
+Количество серверных стоек — 500<br><br>
+Мощность ДЦ, кВТ — 3 300<br><br>
+Источники бесперебойного питания, дизель-генераторы N+1, прямой фрикулинг с адиабатическим доохлаждением`,
+      content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit animLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim`
+    }
+  ],
+  servers: 'Показать доступные сервера'
+}
+</script>
--- pages/reviews.vue
+++ pages/reviews.vue
@@ -5,11 +5,11 @@
         <Nav/>
         <div class="row lead">
           <div class="col-10 col-sm-6 col-lg-7">
-            <h1>Отзывы<br> клиентов</h1>
-            <p>Отзывы наших прекрасных и отзывчивых клиентов</p>
+            <h1 v-html="translate.header.title"></h1>
+            <p>{{ translate.header.description }}</p>
           </div>
           <div class="clients">
-            209 клиентов<br> довольны нами
+            {{ translate.header.clients }} клиентов<br> довольны нами
             <img src="/images/star-gray.png" alt="">
           </div>
         </div>
@@ -20,154 +20,80 @@
         <div class="row">
           <div class="col-12 col-md-4 col-lg-3">
             <div class="card">
-              <a href="#"><img src="/images/hostings-info-logo.png" alt=""></a>
-              <a href="#"><img src="/images/trustpilot-logo.png" alt=""></a>
-              <a href="#"><img src="/images/vk-logo.png" alt=""></a>
+              <a target="_blank" href="https://vk.com/topic-171073991_39268980"><img src="/images/hostings-info-logo.png" alt=""></a>
+              <a target="_blank" href="https://ru.hostings.info/spacecore-pro.html"><img src="/images/vk-logo.png" alt=""></a>
             </div>
             <div class="banner">
               <img class="img-fluid" src="/images/banner.png" alt="">
             </div>
           </div>
           <div class="col-12 col-md-8 col-lg-9">
-            <div class="review">
+            <div class="review" v-for="(el, i) in translate.elements" :key="i">
               <div class="head">
                 <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
+                  <div class="time-site">{{ new Date(el.date).toLocaleDateString() }} • vk.com</div>
+                  <div class="name">{{ el.first_name }} {{ el.last_name }}</div>
                 </div>
-                <div class="stars">
+                <!-- <div class="stars">
                   <div class="star active"></div>
                   <div class="star active"></div>
                   <div class="star active"></div>
                   <div class="star active"></div>
                   <div class="star"></div>
-                </div>
+                </div> -->
               </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
-            </div>
-            <div class="review">
-              <div class="head">
-                <div>
-                  <div class="time-site">12.02.2000 • vk.com</div>
-                  <div class="name">Иванов Иван</div>
-                </div>
-                <div class="stars">
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star active"></div>
-                  <div class="star"></div>
-                </div>
-              </div>
-              <p>Все просто прекрасно! Огромное Вам спасибо, думаю можно теперь вкладываться по полной в FASTVPS, это лучший хостинг сервис который я когда либо пробовала, много что незнакомо и непонятно с первго раза, но уровень и качество технической поддержки, точно лучший из лучших! Еще раз огромное спасибо, мой выбор очевиден, буду советовать знакомым.</p>
-              <a href="#">Подробнее</a>
+              <p>{{ el.text }}</p>
+              <a target="_blank" :href="el.url">{{ translate.more }}</a>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="container feedback">
-        <div class="row">
-          <div class="col-12 col-md-6">
-            <h2 class="title">У вас еще остались <br>вопросы?</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id</p>
-          </div>
-          <div class="col-12 col-md-6 col-lg-5 offset-lg-1">
-            <form action="">
-              <input type="text" placeholder="Ваше имя">
-              <input type="text" placeholder="Ваш Email">
-              <button type="submit">Получить консультацию</button>
-            </form>
-          </div>
-        </div>
-      </section>
+      <Feedback/>
     </main>
     <Footer/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      translate: translate
+    }
+  }
+})
+
+let translate = {
+  header: {
+    title: 'Отзывы<br> клиентов',
+    description: 'Отзывы наших прекрасных и отзывчивых клиентов',
+    clients: 209 // TODO плюрализация
+  },
+  elements: [
+    {
+      date: 1601372291060,
+      first_name: 'Данил',
+      last_name: 'Голощапов',
+      text: 'Приобрёл ВДСку, всё гуд, поддержка отличная, работает без перебоев.',
+      url: '#'
+    },
+    {
+      date: 1601372291060,
+      first_name: 'Данил',
+      last_name: 'Голощапов',
+      text: 'Приобрёл ВДСку, всё гуд, поддержка отличная, работает без перебоев.',
+      url: '#'
+    },
+    {
+      date: 1601372291060,
+      first_name: 'Данил',
+      last_name: 'Голощапов',
+      text: 'Приобрёл ВДСку, всё гуд, поддержка отличная, работает без перебоев.',
+      url: '#'
+    }
+  ],
+  more: 'Подробнее'
+}
+</script>
