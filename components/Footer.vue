5fc4dadf6010f482ca9b48385163dadaf81edb05
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-10-16 21:13:59 +0300

    Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,31 +5,17 @@
         <div class="row">
           <div class="col-12 col-sm-8 col-md-4">
             <img class="img-fluid" src="/images/logo-footer.png" alt="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>{{ translate.description }}</p>
           </div>
           <div class="col-12 col-md-8 col-lg-6 footer-nav">
-            <strong>Навигация</strong>
-            <ul class="list-unstyled">
-              <li><a href="#">Ссылка</a></li>
-              <li><a href="#">Сылка в навигации</a></li>
-              <li><a href="#">Договор офферты</a></li>
-              <li><a href="#">Еще одна ссылка</a></li>
-              <li><a href="#">Ссылка</a></li>
-            </ul>
-            <ul class="list-unstyled">
-              <li><a href="#">Ссылка</a></li>
-              <li><a href="#">Сылка в навигации</a></li>
-              <li><a href="#">Договор офферты</a></li>
-              <li><a href="#">Еще одна ссылка</a></li>
-              <li><a href="#">Ссылка</a></li>
-            </ul>
-            <ul class="list-unstyled">
-              <li><a href="#">Ссылка</a></li>
-              <li><a href="#">Сылка в навигации</a></li>
-              <li><a href="#">Договор офферты</a></li>
-              <li><a href="#">Еще одна ссылка</a></li>
-              <li><a href="#">Ссылка</a></li>
-            </ul>
+            <strong>{{ translate.nav.title }}</strong>
+            <div>
+              <ul class="list-unstyled" v-for="(el, i) in translate.nav.elements" :key="i">
+                <li v-for="(link, j) in el" :key="j">
+                  <NavLink :el="link" />
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="col-12 col-sm-4 col-lg-2 social">
             <a href="#"><img src="/images/twitter.png" alt=""></a>
@@ -41,9 +27,119 @@
     </div>
     <div class="footer-bottom">
       <div class="container">
-        <p>Все права защищены© 20!8 - 2020 SpaceCore / June 15, 2020, 12:07 am</p>
+        <p>{{ translate.copyrights }}</p>
         <img src="/images/pay-systems.png" alt="">
       </div>
     </div>
   </footer>
 </template>
+
+<style lang="sass" scoped>
+.footer-nav>div
+  display: flex
+  flex-wrap: wrap
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import { setTranslate } from './Nav.vue'
+
+export default Vue.extend({
+  computed: {
+    translate() {
+      return setTranslate(this.$store.state.lang, {
+        ru: translate_ru,
+        ua: translate_ua,
+        en: translate_en,
+      });
+    },
+  },
+})
+
+
+const translate_ru = {
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  copyrights: 'Все права защищены© 20!8 - 2020 SpaceCore / June 15, 2020, 12:07 am',
+  nav: {
+    title: 'Навигация',
+    elements: [
+      [
+        {
+          title: "Виртуальные серверы",
+          link: "/vds",
+        },
+        {
+          title: "Выделенные серверы",
+          link: "/dedicated",
+        },
+        {
+          title: "Серверы без установки",
+          link: "/not-install",
+        },
+        {
+          title: "Веб-хостинг",
+          link: "/hosting",
+        },
+        {
+          title: "Домены",
+          link: "/domains",
+        },
+        {
+          title: "Лицензии ISP",
+          link: "/isplicense",
+        }
+      ],
+      [
+        {
+          title: "Виртуальные серверы",
+          link: "/vds",
+        },
+        {
+          title: "Выделенные серверы",
+          link: "/dedicated",
+        },
+        {
+          title: "Серверы без установки",
+          link: "/not-install",
+        },
+        {
+          title: "Веб-хостинг",
+          link: "/hosting",
+        },
+        {
+          title: "Домены",
+          link: "/domains",
+        },
+        {
+          title: "Лицензии ISP",
+          link: "/isplicense",
+        }
+      ],
+      [
+        {
+          title: "Виртуальные серверы",
+          link: "/vds",
+        },
+        {
+          title: "Выделенные серверы",
+          link: "/dedicated",
+        },
+        {
+          title: "Серверы без установки",
+          link: "/not-install",
+        },
+        {
+          title: "Веб-хостинг",
+          link: "/hosting",
+        },
+        {
+          title: "Домены",
+          link: "/domains",
+        }
+      ],
+    ]
+  }
+}
+const translate_ua = {}
+const translate_en = {}
+</script>
