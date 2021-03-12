0b8d2acdbe06439e2ed58fb66e903719befdcc3b
Andrew Molchanov <mr.catmanrus@gmail.com>
2021-03-12 20:52:04 +0300

    Веб-хостинг

--- pages/hosting.vue
+++ pages/hosting.vue
@@ -18,22 +18,21 @@
         <div class="row">
           <div
             class="col-12 col-md-6 col-lg-4"
-            v-for="(el, i) in translate.main.elements" :key="i"
-            :class="i == translate.main.elements.length - 1 ? 'm-auto' : ''"
+            v-for="(el, i) in tariffs" :key="i"
           >
             <div class="hosting-card">
               <div class="head">
                 <strong>{{ el.title }}</strong>
                 <p class="text">{{ el.description }}</p>
                 <p class="storage">{{ el.storage }}</p>
-                <p class="sites">{{ el.sites }}</p>
+                <p class="sites">{{ el.sites }} {{ sitesPlural(el.sites) }}</p>
               </div>
               <ul class="list-unstyled">
-                <li v-for="(text, j) in el.list" :key="j">{{ text }}</li>
+                <li v-for="(text, j) in el.features" :key="j">{{ text }}</li>
               </ul>
               <p class="price">
                 {{ translate.main.price }}
-                <strong>{{ el.price }}</strong>
+                <strong>{{ el.price }} {{ translate.main.currency }}</strong>
               </p>
               <a class="buy" href="#">{{ translate.main.buy }}</a>
             </div>
@@ -51,12 +50,58 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+  data() {
+    return {
+      tariffs: {}
+    }
+  },
+  async asyncData({$axios, store}) {
+    const tariffs = await $axios.$get(`${store.state.lang}/hosting`)
+
+    return {
+      tariffs,
+    }
+  },
+  watch: {
+    translate: 'getTariffs'
+  },
   computed: {
     translate() {
       return this.$getTranslate(translate);
     }
+  },
+  methods: {
+    sitesPlural(count: number): string {
+      return this.$pluralization(count, sitesTranslate)
+    },
+    async getTariffs() {
+      this.tariffs = await this.$axios.$get(`${this.$store.state.lang}/hosting`)
+    }
   }
 })
+
+const sitesTranslate = {
+  ru: {
+    one: "сайт",
+    few: "сайта",
+    many: "сайтов",
+    other: ""
+  },
+  en: {
+    one: "site",
+    few: "",
+    many: "",
+    other: "sites"
+  },
+  ua: {
+    one: "сайт",
+    few: "сайта",
+    many: "сайтов",
+    other: ""
+  }
+}
+
+
 
 const translate = {
   ru: {
@@ -70,52 +115,42 @@ const translate = {
     },
     main: {
       title: 'Хостинг сайтов',
-      elements: [
-        {
-          title: 'host-0',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-          storage: '13 ГБ SSD',
-          sites: '7 сайтов',
-          list: [
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта'
-          ],
-          price: '5364 rub'
-        },
-        {
-          title: 'host-0',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-          storage: '13 ГБ SSD',
-          sites: '7 сайтов',
-          list: [
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта'
-          ],
-          price: '5364 rub'
-        },
-        {
-          title: 'host-0',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
-          storage: '13 ГБ SSD',
-          sites: '7 сайтов',
-          list: [
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта',
-            'Поддержка php / mysql и многое другое для сайта'
-          ],
-          price: '5364 rub'
-        }
-      ],
       price: 'Цена',
+      currency: '₽',
+      buy: 'Оформить заказ',
+    }
+  },
+  ua: {
+    header: {
+      title: 'Виртуальный хостинг',
+      description: 'Виртуальный SSD хостинг для сайтов любой сложности.<br> Вы можете разместить от личной визитки до высоконагруженных проектов',
+      button: {
+        text: 'Просмотреть услуги',
+        href: '#'
+      }
+    },
+    main: {
+      title: 'Хостинг сайтов',
+      price: 'Цена',
+      currency: '₴',
       buy: 'Оформить заказ'
     }
   },
-  ua: {},
-  en: {}
+  en: {
+    header: {
+      title: 'Виртуальный хостинг',
+      description: 'Виртуальный SSD хостинг для сайтов любой сложности.<br> Вы можете разместить от личной визитки до высоконагруженных проектов',
+      button: {
+        text: 'Просмотреть услуги',
+        href: '#'
+      }
+    },
+    main: {
+      title: 'Хостинг сайтов',
+      price: 'Цена',
+      currency: '€',
+      buy: 'Оформить заказ'
+    }
+  }
 }
 </script>
